ccb2bf2a4f4232fcecf17b9a82d4cb8dd13f85c6
eric <ericwang27759@gmail.com>
2024-05-31 15:26:06 -0700

    added catch-all route for news archive

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import NewsList from "@/components/news-list";
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
+
+export default function FilteredNewsPage({ params }) {
+    const filter = params.filter || [];
+    const [selectedYear, selectedMonth] = filter;
+    let news;
+    let links = getAvailableNewsYears();
+    let newsContent = <p>No news found for the selected period.</p>;
+
+    if (selectedYear && !selectedMonth) {
+        news = getNewsForYear(selectedYear);
+        links = getAvailableNewsMonths(selectedYear);
+    }
+
+    if (selectedYear && selectedMonth) {
+        news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+        links = [];
+    }
+
+    if (news && news.length > 0) {
+        newsContent = <NewsList news={news} />;
+    }
+
+    return (
+        <>
+            <header id="archive-header">
+                <nav>
+                    <ul>
+                        {
+                            links.map(link => {
+                                const href = `/archive/${selectedYear ? `${selectedYear}/` : ''}${link}`;
+                                
+                                return (
+                                    <li key={link}>
+                                        <Link href={href}>{link}</Link>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </nav>
+            </header>
+            {newsContent}
+        </>
+    );
+}
